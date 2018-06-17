873af6b3a672b1bc73673f4f7d1f4b7913932693
jennifer <jenn.chen@live.ca>
2018-06-17 13:29:18 -0700

    Make dots bigger on hover

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function clickListener(event){
   svg.setAttribute("viewbox", `${b.x} ${b.y} ${b.width} ${b.height}`)
 }
 
+// width="1500"
+// height="1266.066"
+// viewBox="0 0 1500 1266.066"
+// viewBox="0 0 971.84008 820.27557"
+
 document.addEventListener('DOMContentLoaded', () => {
   const dots = document.querySelectorAll('circle')
   dots.forEach(element => {
@@ -18,6 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   dots.forEach(el => {
     el.addEventListener("mouseenter", event => {
       const {currentTarget} = event;
+      currentTarget.setAttribute("r", 4)
       currentTarget.classList.add('pointHover')
     })
   })
@@ -25,6 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
   dots.forEach(el => {
     el.addEventListener("mouseleave", event => {
       const {currentTarget} = event;
+      currentTarget.setAttribute("r", 1.4173229)
       currentTarget.classList.remove('pointHover')
     })
   })
